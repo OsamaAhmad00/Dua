93be95b4ceb5cdb1c22a7094e2c10319a8125c99
Osama Ahmad <osama199915@gmail.com>
2023-11-07 10:55:24 +0200

    Support float types

---
+++ include/types/FloatTypes.h
@@ -0,0 +1,18 @@
+#include <types/TypeBase.h>
+
+#define DEFINE_FLOAT_TYPE(WIDTH, TYPE)                \
+struct F##WIDTH##Type : public FloatType              \
+{                                                     \
+    llvm::Constant* default_value() override {        \
+        return llvm::ConstantFP::get(llvm_type(), 0); \
+    }                                                 \
+                                                      \
+    llvm::Type* llvm_type() override {                \
+        return builder->get##TYPE##Ty();              \
+    }                                                 \
+};
+
+struct FloatType : public TypeBase {};
+
+DEFINE_FLOAT_TYPE(64, Double)
+DEFINE_FLOAT_TYPE(32, Float)
