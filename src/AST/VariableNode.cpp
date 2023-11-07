72eaa5204f7fd5c1c63834cbcc8a59b500783b5d
Osama Ahmad <osama199915@gmail.com>
2023-11-08 00:57:05 +0200

    improve the VariableNode::eval function

--- src/AST/VariableNode.cpp
+++ src/AST/VariableNode.cpp
@@ -2,10 +2,11 @@
 
 llvm::Value* VariableNode::eval()
 {
-    auto variable = symbol_table().contains(name) ?
-            symbol_table().get(name) : symbol_table().get_global(name);
-    llvm::Value* result = variable.ptr;
-    if (dereference)
-        result = builder().CreateLoad(variable.type, result);
-    return result;
+    // This searches locally first, then globally if not found.
+    auto variable = symbol_table().get(name);
+
+    if (get_address)
+        return variable.ptr;
+
+    return builder().CreateLoad(variable.type, variable.ptr);
 }
