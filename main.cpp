a1414ad6e50120b6cfb87ecb52f4245438b2ce81
Osama Ahmad <osama199915@gmail.com>
2023-11-14 20:26:22 +0200

    Support unary -, +, !, and ~ operators

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main()
         "    int i = 4;"
         "    int j = 1;"
         "    if (i <= 4) {"
-        "        j = 5;"
-        "        if (j > 10) {"
+        "        j = -5;"
+        "        if (!(j > 10)) {"
         "            i = 30;"
         "        } else"
         "            i = 3;"
         "    }"
-        "    return i * j;"
+        "    return ~(i * j);"
         "}"
     );
 }
