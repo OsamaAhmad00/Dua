446f330759fd006e31abe55cb51129b0e55bcd2e
Osama Ahmad <osama199915@gmail.com>
2023-11-14 21:31:27 +0200

    Set the comparison nodes to cast rhs to the type of lhs before comparison

--- include/AST/binary/BinaryExp.h
+++ include/AST/binary/BinaryExp.h
@@ -22,3 +22,30 @@ public:                                                            \
         delete rhs;                                                \
     }                                                              \
 };
+
+#define SAME_TYPE_BINARY_EXP_NODE(NAME, OP, LABEL)                                  \
+class NAME : public ASTNode                                                         \
+{                                                                                   \
+    ASTNode* lhs;                                                                   \
+    ASTNode* rhs;                                                                   \
+                                                                                    \
+public:                                                                             \
+                                                                                    \
+    NAME(ModuleCompiler* compiler, ASTNode* lhs, ASTNode* rhs)                      \
+        : lhs(lhs), rhs(rhs) { this->compiler = compiler; }                         \
+                                                                                    \
+    llvm::Value* eval() override {                                                  \
+        auto lhs_value = lhs->eval();                                               \
+        auto rhs_value = rhs->eval();                                               \
+        rhs_value = compiler->cast_value(rhs_value, lhs_value->getType());          \
+        if (rhs_value == nullptr)                                                   \
+            throw std::runtime_error("Type mismatch between the two operands");     \
+        return builder().OP(lhs_value, rhs_value, LABEL);                           \
+    }                                                                               \
+                                                                                    \
+    ~NAME() override {                                                              \
+        delete lhs;                                                                 \
+        delete rhs;                                                                 \
+    }                                                                               \
+};
+
