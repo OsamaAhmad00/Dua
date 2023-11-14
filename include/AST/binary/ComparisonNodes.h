4a61eb87a651e2e1fba55768a16046ac351c0a8d
Osama Ahmad <osama199915@gmail.com>
2023-11-14 21:31:27 +0200

    Set the comparison nodes to cast rhs to the type of lhs before comparison

--- include/AST/binary/ComparisonNodes.h
+++ include/AST/binary/ComparisonNodes.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#include <AST/binary/BinaryExp.h>
+#include <AST/ASTNode.h>
 
-BINARY_EXP_NODE(LTNode, CreateICmpSLT, "temp_lt")
+SAME_TYPE_BINARY_EXP_NODE(LTNode, CreateICmpSLT, "temp_lt")
 
-BINARY_EXP_NODE(GTNode, CreateICmpSGT, "temp_gt")
+SAME_TYPE_BINARY_EXP_NODE(GTNode, CreateICmpSGT, "temp_gt")
 
-BINARY_EXP_NODE(LTENode, CreateICmpSLE, "temp_lte")
+SAME_TYPE_BINARY_EXP_NODE(LTENode, CreateICmpSLE, "temp_lte")
 
-BINARY_EXP_NODE(GTENode, CreateICmpSGE, "temp_gte")
+SAME_TYPE_BINARY_EXP_NODE(GTENode, CreateICmpSGE, "temp_gte")
 
-BINARY_EXP_NODE(EQNode, CreateICmpEQ, "temp_eq")
+SAME_TYPE_BINARY_EXP_NODE(EQNode, CreateICmpEQ, "temp_eq")
 
-BINARY_EXP_NODE(NENode, CreateICmpNE, "temp_ne")
+SAME_TYPE_BINARY_EXP_NODE(NENode, CreateICmpNE, "temp_ne")
