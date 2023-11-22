1aa40962514dda4138954455bf28ec983f83ddd2
Osama Ahmad <osama199915@gmail.com>
2023-11-22 21:39:22 +0200

    Add if and when expressions tests

---
+++ testing/tests/IfExpressions.cpp
@@ -0,0 +1,10 @@
+#include "FileTestCasesRunner.h"
+
+namespace dua
+{
+
+TEST(if_expressions, if_expressions) {
+    FileTestCasesRunner("if-expressions.dua").run();
+}
+
+}
---
+++ testing/tests/WhenExpressions.cpp
@@ -0,0 +1,10 @@
+#include "FileTestCasesRunner.h"
+
+namespace dua
+{
+
+TEST(when_expressions, when_expressions) {
+    FileTestCasesRunner("when-expressions.dua").run();
+}
+
+}
