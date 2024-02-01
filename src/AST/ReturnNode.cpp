80dd4dc0d96df298ee82f35a0e2f02d355796049
Osama Ahmad <osama199915@gmail.com>
2024-02-01 06:50:34 +0200

    Report an error when a non-void function returns void

--- src/AST/ReturnNode.cpp
+++ src/AST/ReturnNode.cpp
@@ -1,18 +1,22 @@
 #include <AST/function/ReturnNode.hpp>
 #include <types/VoidType.hpp>
-#include "types/PointerType.hpp"
 
 namespace dua
 {
 
 Value ReturnNode::eval()
 {
-    if (expression == nullptr)
-        return compiler->create_value(builder().CreateRetVoid(), compiler->create_type<VoidType>());
-
     auto func = current_function()->getName().str();
-    auto result = expression->eval();
     auto return_type = name_resolver().get_function_no_overloading(func).type->return_type;
+
+    if (expression == nullptr) {
+        auto void_type = compiler->create_type<VoidType>();
+        if (return_type != void_type)
+            report_error("Can't return void from the function " + func + " which returns a " + return_type->to_string());
+        return compiler->create_value(builder().CreateRetVoid(), void_type);
+    }
+
+    auto result = expression->eval();
 
     auto ptr = typing_system().cast_value(result, return_type).get();
 
